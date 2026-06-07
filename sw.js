336908e09d6cb56b11c067fe0895c94dcc3b06ee
kmog8888-cmyk <kmog.8888@gmail.com>
2026-06-08 06:28:23 +0900

    Add Korea travel PWA

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'korea-travel-v1';
+const ASSETS = [
+  './korea_travel.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Pass through non-GET and external requests
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match('./korea_travel.html'));
+    })
+  );
+});
